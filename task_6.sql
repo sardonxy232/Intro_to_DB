13c97179808d0fe60c9229b441cc068aec6f6ebe
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-06-28 16:27:16 +0100

    up

--- task_6.sql
+++ task_6.sql
@@ -29,7 +29,7 @@
 -- GitHub repository: Intro_to_DB
 -- File: task_6.sql
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer(customer_id, customer_name, email, address)
 VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
