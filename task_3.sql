470079b778b2ca2bad008c588da3489b9a475602
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-06-28 16:59:42 +0100

    updated

--- task_3.sql
+++ task_3.sql
@@ -4,3 +4,12 @@
 
 -- The name of the file should be task_3.sql
 -- The database name will be passed as argument of mysql command
+-- Repo:
+
+-- GitHub repository: Intro_to_DB
+-- File: task_3.sql;
+USE alx_book_store;
+SELECT TABLE_NAME
+
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'alx_book_store';
